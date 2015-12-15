4d3d7b67f90534ce66a02f1712fdb88769617940
enternoescape <dcagent17@yahoo.com>
2015-12-15 09:39:22 -0500

    Added some helpful hints to post installation.

--- scripts/deb/postInstall.sh
+++ scripts/deb/postInstall.sh
@@ -4,6 +4,11 @@ chown opendct:opendct /opt/opendct
 ln -fs /opt/opendct/service /etc/init.d/opendct
 chmod 755 /etc/init.d/opendct
 
+echo "To use the provided ufw rules type:"
+echo "/opt/opendct/enable-ufw-ports"
+echo ""
+echo "To delete the provided ufw rules type:"
+echo "/opt/opendct/disable-ufw-ports"
 echo ""
 echo "To start the OpenDCT service type:"
 echo "service opendct start"

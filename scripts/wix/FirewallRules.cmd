604b03d39406137e18eb389c5ed5c292405c8dcc
enternoescape <dcagent17@yahoo.com>
2015-12-15 15:15:04 -0500

    Added a batch file to create the firewall changes because the WiX script is complaining about FirewallException not existing. This will need to suffice until I figure out what's going on.

---
+++ scripts/wix/FirewallRules.cmd
@@ -0,0 +1,4 @@
+netsh advfirewall firewall add rule name="OpenDCT (SageTV Media Server)" dir=out localport=7818 protocol=TCP action=allow
+netsh advfirewall firewall add rule name="OpenDCT (SageTV Network Encoder)" dir=in localport=9000-9100 protocol=TCP action=allow
+netsh advfirewall firewall add rule name="OpenDCT (SageTV Discovery)" dir=in localport=8271 protocol=UDP action=allow
+netsh advfirewall firewall add rule name="OpenDCT (RTP)" dir=in localport=8300-8500 protocol=UDP action=allow

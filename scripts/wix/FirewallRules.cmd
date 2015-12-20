c6e6ebc8a72077612915e9c9c43b1987bc27e51b
enternoescape <dcagent17@yahoo.com>
2015-12-20 03:22:20 -0500

    Added wrapper.exe to firewall exceptions on the Windows installer.

--- scripts/wix/FirewallRules.cmd
+++ scripts/wix/FirewallRules.cmd
@@ -1,3 +1,13 @@
+IF EXIST "C:\Program Files (x86)\OpenDCT Authors\OpenDCT\jsw\bin\wrapper.exe" (
+	netsh advfirewall firewall add rule name="OpenDCT (JSW)" dir=in action=allow program="C:\Program Files (x86)\OpenDCT Authors\OpenDCT\jsw\bin\wrapper.exe"
+	netsh advfirewall firewall add rule name="OpenDCT (JSW)" dir=out action=allow program="C:\Program Files (x86)\OpenDCT Authors\OpenDCT\jsw\bin\wrapper.exe"
+)
+
+IF EXIST "C:\Program Files\OpenDCT Authors\OpenDCT\jsw\bin\wrapper.exe" (
+	netsh advfirewall firewall add rule name="OpenDCT (JSW)" dir=in action=allow program="C:\Program Files\OpenDCT Authors\OpenDCT\jsw\bin\wrapper.exe"
+	netsh advfirewall firewall add rule name="OpenDCT (JSW)" dir=out action=allow program="C:\Program Files\OpenDCT Authors\OpenDCT\jsw\bin\wrapper.exe"
+)
+
 netsh advfirewall firewall add rule name="OpenDCT (SageTV Media Server)" dir=out localport=7818 protocol=TCP action=allow
 netsh advfirewall firewall add rule name="OpenDCT (SageTV Network Encoder)" dir=in localport=9000-9100 protocol=TCP action=allow
 netsh advfirewall firewall add rule name="OpenDCT (SageTV Discovery)" dir=in localport=8271 protocol=UDP action=allow

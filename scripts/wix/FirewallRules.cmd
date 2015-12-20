88f13a46443723d9164f60d92b77d2d536099079
enternoescape <dcagent17@yahoo.com>
2015-12-20 04:58:41 -0500

    Added wrapper.exe to firewall exceptions on the Windows installer.

--- scripts/wix/FirewallRules.cmd
+++ scripts/wix/FirewallRules.cmd
@@ -1,15 +1,21 @@
-IF EXIST "C:\Program Files (x86)\OpenDCT Authors\OpenDCT\jsw\bin\wrapper.exe" (
-	netsh advfirewall firewall add rule name="OpenDCT (JSW)" dir=in action=allow program="C:\Program Files (x86)\OpenDCT Authors\OpenDCT\jsw\bin\wrapper.exe"
-	netsh advfirewall firewall add rule name="OpenDCT (JSW)" dir=out action=allow program="C:\Program Files (x86)\OpenDCT Authors\OpenDCT\jsw\bin\wrapper.exe"
+IF EXIST %1 (
+	rem Re-create this rule every time in case the install directory changed.
+	netsh advfirewall firewall delete rule name="OpenDCT (JSW)"
+	netsh advfirewall firewall add rule name="OpenDCT (JSW)" dir=in action=allow program=%1
+	netsh advfirewall firewall add rule name="OpenDCT (JSW)" dir=out action=allow program=%1
 )
 
-IF EXIST "C:\Program Files\OpenDCT Authors\OpenDCT\jsw\bin\wrapper.exe" (
-	netsh advfirewall firewall add rule name="OpenDCT (JSW)" dir=in action=allow program="C:\Program Files\OpenDCT Authors\OpenDCT\jsw\bin\wrapper.exe"
-	netsh advfirewall firewall add rule name="OpenDCT (JSW)" dir=out action=allow program="C:\Program Files\OpenDCT Authors\OpenDCT\jsw\bin\wrapper.exe"
-)
+netsh advfirewall firewall show rule name="OpenDCT (SageTV Media Server)" >nul
+IF ERRORLEVEL 1 netsh advfirewall firewall add rule name="OpenDCT (SageTV Media Server)" dir=out localport=7818 protocol=TCP action=allow
 
-netsh advfirewall firewall add rule name="OpenDCT (SageTV Media Server)" dir=out localport=7818 protocol=TCP action=allow
-netsh advfirewall firewall add rule name="OpenDCT (SageTV Network Encoder)" dir=in localport=9000-9100 protocol=TCP action=allow
-netsh advfirewall firewall add rule name="OpenDCT (SageTV Discovery)" dir=in localport=8271 protocol=UDP action=allow
-netsh advfirewall firewall add rule name="OpenDCT (RTP)" dir=in localport=8300-8500 protocol=UDP action=allow
-netsh advfirewall firewall add rule name="OpenDCT (Cling HTTP TCP)" dir=in localport=8501 protocol=TCP action=allow
+netsh advfirewall firewall show rule name="OpenDCT (SageTV Network Encoder)" >nul
+IF ERRORLEVEL 1 netsh advfirewall firewall add rule name="OpenDCT (SageTV Network Encoder)" dir=in localport=9000-9100 protocol=TCP action=allow
+
+netsh advfirewall firewall show rule name="OpenDCT (SageTV Discovery)" >nul
+IF ERRORLEVEL 1 netsh advfirewall firewall add rule name="OpenDCT (SageTV Discovery)" dir=in localport=8271 protocol=UDP action=allow
+
+netsh advfirewall firewall show rule name="OpenDCT (RTP)" >nul
+IF ERRORLEVEL 1 netsh advfirewall firewall add rule name="OpenDCT (RTP)" dir=in localport=8300-8500 protocol=UDP action=allow
+
+netsh advfirewall firewall show rule name="OpenDCT (Cling HTTP TCP)" >nul
+IF ERRORLEVEL 1 netsh advfirewall firewall add rule name="OpenDCT (Cling HTTP TCP)" dir=in localport=8501 protocol=TCP action=allow

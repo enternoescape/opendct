4c6db91f89dafdd6651aa6cf81ac6d83298a5cdf
enternoescape <dcagent17@yahoo.com>
2015-12-24 15:55:37 -0500

    Fixed copyright header.

--- src/main/java/opendct/jetty/WebDivBuilders.java
+++ src/main/java/opendct/jetty/WebDivBuilders.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2015 The OpenDCT Authors. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package opendct.jetty;
 
 import opendct.capture.CaptureDevice;
@@ -20,7 +36,7 @@ public class WebDivBuilders {
         out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR/html4/strict.dtd\">\n" +
                 "<html>\n" +
                 "<head>\n" +
-                "   <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js\"></script>" +
+                "   <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js\"></script>\n" +
                 "   <link rel=\"stylesheet\" type=\"text/css\" href=\"css/opendct.css\"/>\n" +
                 "   <title>OpenDCT Web Interface</title>\n" +
                 "</head>\n" +
@@ -47,13 +63,18 @@ public class WebDivBuilders {
             return;
         }
 
-        out.println("<div class=\"loaded_devices_table\"><table class=\"loaded_devices\">");
+        out.println("<form><div class=\"loaded_devices_table\">");
+        out.println("<select name=\"p\"");
+        out.println("<option value=\"1\" selected>Option 1</option>");
+        out.println("<option value=\"2\"         >Option 2</option>");
+        out.println("</select>");
+        out.println("<table class=\"loaded_devices\">");
 
         for (CaptureDevice captureDevice : captureDevices) {
             printCaptureDeviceCell(captureDevice.getEncoderName(), out);
         }
 
-        out.println("</table></div>");
+        out.println("</table></div></form>");
         out.println("<hr/>");
     }
 

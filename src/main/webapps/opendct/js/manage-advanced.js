d3d13ae2c6f67570a22c6d1f27492272147f1aa8
enternoescape <dcagent17@yahoo.com>
2016-01-01 14:21:45 -0500

    More advanced settings work.

--- src/main/webapps/opendct/js/manage-advanced.js
+++ src/main/webapps/opendct/js/manage-advanced.js
@@ -7,7 +7,7 @@ var manageEditDevices = [];
 
 function showManageAdvanced( requester ) {
     $("#manage-basic").hide();
-    $("#manage-advanced").show();
+    $("#manage-advanced-devices").empty();
 
     // Select create a list of the devices we are going to edit.
     if ($(requester).parent().parent().find(".manage-loaded-checkbox:checked").length == 0) {
@@ -20,16 +20,20 @@ function showManageAdvanced( requester ) {
         });
     }
 
-    console.log(manageEditDevices);
+    $.each(manageEditDevices, function(i, deviceName) {
+        $("#manage-advanced-devices").append(deviceName);
+        $("#manage-advanced-devices").append('<br/>');
+    });
+
+    $("#manage-advanced").show();
 }
 
 function hideManageAdvanced() {
+    $("#manage-advanced").hide();
+
     // This ensures that if the advanced options are not displayed, we can't accidentally apply
     // settings in some unexpected way.
     manageEditDevices = [];
 
     $("#manage-basic").show();
-    $("#manage-advanced").hide();
-
-    console.log(manageEditDevices);
 }

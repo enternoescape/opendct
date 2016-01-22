831444b9b3181471c9a5450c976fbbaa44feee59
enternoescape <dcagent17@yahoo.com>
2016-01-22 09:50:24 -0500

    Upgraded Linux packaging solution and added scripting to handle the configuration location changes automatically during install.

--- scripts/rpm/postInstall.sh
+++ scripts/rpm/postInstall.sh
@@ -23,6 +23,39 @@ chown opendct:opendct /opt/opendct
 # This should fix a potential SELinux issue.
 restorecon /etc/firewalld/services/opendct.xml
 
+##### 0.4.18 Upgrade START#####
+if test ! -e /etc/opendct/conf; then
+    mkdir -p /etc/opendct/conf
+fi
+
+chown opendct:opendct /etc/opendct/conf
+
+if test -e /opt/opendct/conf; then
+    echo ""
+    echo "Copying the files in the directory /opt/opendct/conf/* to /etc/opendct/conf/..."
+
+    # -n option is used in case the user already moved or was confused about the changes and renames
+    # the file we're checking back. This way we don't overwrite settings actually in use.
+    cp -rvn /opt/opendct/conf/* /etc/opendct/conf/
+    mv /opt/opendct/conf /opt/opendct/conf.moved
+
+    # There is also a problem that is the reason for this change whereby the opendct.properties file
+    # might be removed/replaced as a part of the upgrade. This uses the last backup copy to attempt
+    # to fix the problem.
+    if test ! -e /opt/opendct/conf.moved/opendct.properties && test -e /opt/opendct/conf.moved/opendct.properties.backup; then
+        echo "Attempting to restore /etc/opendct/conf/opendct.properties from backup..."
+        cp -vf /opt/opendct/conf.moved/opendct.properties.backup /etc/opendct/conf/opendct.properties
+    fi
+
+    echo ""
+    echo "Successfully copied the files."
+    echo "All configuration data is now located in the folder /etc/opendct/conf"
+    echo ""
+    echo "You can safely delete the old configuration folder /opt/opendct/conf.moved"
+    echo ""
+fi
+##### 0.4.18 Upgrade END #####
+
 echo "To use the provided firewalld rules type:"
 echo "firewall-cmd --get-default-zone"
 echo "firewall-cmd --permanent --zone=<default_zone> --add-service=opendct"

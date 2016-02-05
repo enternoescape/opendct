462ff1c6b90c9ca4bc34e5e97efc79f308cf97ca
enternoescape <dcagent17@yahoo.com>
2016-02-05 09:39:30 -0500

    Automatically add network devices to dependent interfaces and some logging for unexpected situations.

--- src/main/java/opendct/tuning/discovery/DeviceLoaderImpl.java
+++ src/main/java/opendct/tuning/discovery/DeviceLoaderImpl.java
@@ -18,6 +18,7 @@ package opendct.tuning.discovery;
 
 import opendct.capture.CaptureDevice;
 import opendct.capture.CaptureDeviceIgnoredException;
+import opendct.power.NetworkPowerEventManger;
 import opendct.sagetv.SageTVManager;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -36,14 +37,41 @@ public class DeviceLoaderImpl implements DeviceLoader {
         try {
             CaptureDevice captureDevice = discovery.loadCaptureDevice(details.getId());
             SageTVManager.addCaptureDevice(captureDevice);
+
+            DiscoveredDeviceParent parent = discovery.getDeviceParentDetails(details.getParentId());
+
+            if (parent != null) {
+                if (parent.isNetworkDevice()) {
+                    if (parent.getRemoteAddress() == null) {
+                        logger.warn("The capture device parent '{}' reports that it is a network" +
+                                " device, but does not have a remote address.",
+                                parent.getName());
+
+                    } else {
+                        NetworkPowerEventManger.POWER_EVENT_LISTENER.addDependentInterface(parent.getRemoteAddress());
+                    }
+                }
+            } else {
+                logger.warn("The capture device '{}' does not have a parent.",
+                        details.getName());
+
+            }
         } catch (CaptureDeviceIgnoredException e) {
-            logger.error("Not permitted to load the capture device '{}', id {} => ", details.getName(), details.getId(), e);
+            logger.error("Not permitted to load the capture device '{}', id {} => ",
+                    details.getName(), details.getId(), e);
+
         } catch (CaptureDeviceLoadException e) {
-            logger.error("Unable to load the capture device '{}', id {} => ", details.getName(), details.getId(), e);
+            logger.error("Unable to load the capture device '{}', id {} => ",
+                    details.getName(), details.getId(), e);
+
         } catch (SocketException e) {
-            logger.error("Unable to open a socket for the capture device '{}', id {} => ", details.getName(), details.getId(), e);
+            logger.error("Unable to open a socket for the capture device '{}', id {} => ",
+                    details.getName(), details.getId(), e);
+
         } catch (Exception e) {
-            logger.error("Unexpected exception created by the capture device '{}', id {} => ", details.getName(), details.getId(), e);
+            logger.error("Unexpected exception created by the capture device '{}', id {} => ",
+                    details.getName(), details.getId(), e);
+
         }
     }
 

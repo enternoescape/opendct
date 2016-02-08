6f1487801105a3de6bc8e3f1a34f354918638586
enternoescape <dcagent17@yahoo.com>
2016-02-08 07:41:13 -0500

    Typo.

--- src/main/java/opendct/capture/DCTCaptureDeviceImpl.java
+++ src/main/java/opendct/capture/DCTCaptureDeviceImpl.java
@@ -365,7 +365,7 @@ public class DCTCaptureDeviceImpl extends RTPCaptureDevice implements CaptureDev
 
         // This means the lock was already set
         if (this.locked.getAndSet(locked) == locked) {
-            logger.info("Capture device is was already {}.", (locked ? "locked" : "unlocked"));
+            logger.info("Capture device was already {}.", (locked ? "locked" : "unlocked"));
             return false;
         }
 
@@ -553,7 +553,7 @@ public class DCTCaptureDeviceImpl extends RTPCaptureDevice implements CaptureDev
      *         again on a different capture device since this one is currently locked.
      */
     public boolean getChannelInfoOffline(TVChannel tvChannel) {
-        logger.entry();
+        logger.entry(tvChannel);
 
         if (isLocked() || isExternalLocked()) {
             return logger.exit(false);

de491c03080744f2cbb18c74aa9b291d7ce97adb
enternoescape <dcagent17@yahoo.com>
2017-01-18 17:04:45 -0500

    Added landing page for JSON service.

--- src/main/java/opendct/nanohttpd/NanoServlet.java
+++ src/main/java/opendct/nanohttpd/NanoServlet.java
@@ -29,6 +29,9 @@ public class NanoServlet extends RouterNanoHTTPD {
     public void addMappings() {
         super.addMappings();
 
+        // GET: Landing page.
+        addRoute("/", RootServlet.Get.class);
+
         // GET: Get program version, host OS and path information.
         addRoute("/version", VersionJsonServlet.List.class);
 
